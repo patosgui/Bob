2a62f45e038ba255ec2fd542d6125355da9947ec
patosgui <pedro.fraiao2@gmail.com>
2024-03-02 21:01:24 +0100

    Update createenv.ps1
    Fix problem with the location of venv

--- createenv.ps1
+++ createenv.ps1
@@ -1,5 +1,8 @@
+# Need to change cwd otherwise, cannot create the venv
+Set-Location -Path $PSScriptRoot
 # Create a virtual environment
 python3 -m venv env
+
 # Allow the venv activate script to overwrite the environment variables
 Set-ExecutionPolicy Unrestricted -Scope Process
 # Activate the venv
@@ -9,4 +12,4 @@ Write-Output "Location of the python binary"
 gcm python
 
 # Install the requirements
-pip3 install -r requirements.txt
+pip3 install -r $PSScriptRoot\requirements.txt
