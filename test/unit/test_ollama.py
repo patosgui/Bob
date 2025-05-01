1b4e21d6c92d5cbd6e72b9220378eb277612906a
Jose Lopes <lopes.joseps@gmail.com>
2025-05-01 12:55:37 +0200

    refactoring

--- test/unit/test_ollama.py
+++ test/unit/test_ollama.py
@@ -17,44 +17,48 @@ def test_turn_light_on(turn_light_on_mock):
 
     ol_test.client = mock.Mock()
 
-    function_call = Function(arguments='{"location":"0"}', name="turn_light_on")
-    # Create the ChatCompletionMessageToolCall object
-    tool_call = ChatCompletionMessageToolCall(
-        id="call_xip8pu1b", function=function_call, type="function", index=0
-    )
-
-    # Create the ChatCompletionMessage object
-    message = ChatCompletionMessage(
-        content="", role="assistant", tool_calls=[tool_call]
-    )
-
-    # Create the Choice object
-    choice = Choice(finish_reason="tool_calls", index=0, message=message)
-
-    second_choice = Choice(
-        finish_reason="stop",
+    # Create the Choice for function call
+    choice = Choice(
+        finish_reason="tool_calls",
         index=0,
-        logprobs=None,
         message=ChatCompletionMessage(
-            content="How can I assist you further with your home automation needs?",
-            refusal=None,
+            content="",
             role="assistant",
-            annotations=None,
-            audio=None,
-            function_call=None,
-            tool_calls=None,
+            tool_calls=[
+                ChatCompletionMessageToolCall(
+                    id="call_xip8pu1b",
+                    function=Function(
+                        arguments='{"location":"0"}', name="turn_light_on"
+                    ),
+                    type="function",
+                    index=0,
+                )
+            ],
         ),
     )
 
-    mock_obj = mock.MagicMock()
-    mock_obj.choices.__getitem__.return_value = choice
+    # Create the Choice to stop the recursion and the interaction
+    second_choice = Choice(
+        finish_reason="stop",
+        index=0,
+        message=ChatCompletionMessage(
+            content="How can I assist you further with your home automation needs?",
+            role="assistant",
+        ),
+    )
 
-    mock_obj_2 = mock.MagicMock()
-    mock_obj_2.choices.__getitem__.return_value = second_choice
-    ol_test.client.chat.completions.create.side_effect = [mock_obj, mock_obj_2]
+    mock_obj_first = mock.MagicMock()
+    mock_obj_first.choices.__getitem__.return_value = choice
+
+    mock_obj_second = mock.MagicMock()
+    mock_obj_second.choices.__getitem__.return_value = second_choice
+    ol_test.client.chat.completions.create.side_effect = [
+        mock_obj_first,
+        mock_obj_second,
+    ]
 
     turn_light_on_mock.return_value = "Light turned on"
 
-    print(ol_test.analyze(sequence="Can you turn the light on in the office?"))
+    ol_test.analyze(sequence="Can you turn the light on in the office?")
 
     assert turn_light_on_mock.called
